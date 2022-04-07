9b6c7ab09049ad020596fe6257bb19b9b0a39c03
EdvaldoJose <edvaldojose003@gmail.com>
2022-04-06 22:39:03 -0300

    Atualizações dos exercicios/4.1

---
+++ scripts1.js
@@ -0,0 +1,28 @@
+let age = 20;
+age = 30;
+let fullName = "Hulk";
+const age2 = 40;
+
+console.log(age);
+console.log(fullName);
+console.log(age2);
+
+const nyName = 'EdvaldoJosé';
+const birthCity = 'Salvador';
+let birthYear = '1972';
+console.log(nyName, birthYear, birthCity);
+
+let movie = "Avengers"; //string
+let score = 10; //number
+let isValid = true; //boolean
+let name; //undefined
+let color = null; //redefinição
+
+let salary = 3500;
+console.log(salary + salary);
+console.log(salary * salary);
+
+salary++;
+console.log(salary);
+
+
---
+++ scripts2.js
@@ -0,0 +1,15 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+    'firstName: Edvaldo',
+    'lastName:  Santos',
+};
+console.log(typeof patentAge);
+console.log(typeof patientId);
+console.log(patientInfo);
+
+const base = 5;
+const height = 8;
+const area = base * height;
+console.log(area);
+
