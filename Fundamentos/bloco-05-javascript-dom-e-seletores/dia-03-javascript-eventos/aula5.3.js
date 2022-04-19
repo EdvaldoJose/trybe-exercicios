d94e76d7f45fcd09b98a13535a6db00ab0a9f8b0
EdvaldoJose <edvaldojose003@gmail.com>
2022-04-18 22:43:11 -0300

    Atualização dos exercicícios

---
+++ Fundamentos/bloco-05-javascript-dom-e-seletores/dia-03-javascript-eventos/aula5.3.js
@@ -0,0 +1,42 @@
+   
+
+    window.onload = function() {
+        document.querySelector('p').style.color = 'yelloow';
+        document.body.style.backgroundColor = "blue";
+        let p = document.createElement("p");;
+        p.innerHTML = "SOS";
+        document.body.appendChild(p);
+        p.style.textAlign = "center";
+        p.style.color = "white";
+        p.style.fontFamily = "sans-serif";;
+        p.style.fontSize = "14em"
+
+        // let botao = document.getElementById('botaozinho');
+        // botao.adddElementListener('click',function() {
+        //     console.log('hoje está um dia bonito');
+        //     document.style.backgroundColor = 'red';
+        // });
+        //     botao.addEventListener('click',cinquentaTonsDeVerde);
+
+    }
+
+//carrindo de corrida //pegar com Noel os arquivos html e css desse jogo
+
+//     let botao = document.getElementById('star-race-btn');
+//         botao.adddElementListener('click',function(event) {
+//             console.log(event.target);
+    
+        
+//         let car1 = document.querySelector('.car1');
+//         let car2 = document.querySelector('.car2');
+
+//         car1.style.marginLeft += '50px';
+//         car1.style.marginleft = (Math.floor.random() * 100) + "px";
+//         car1.style.marginleft = (Math.floor.random() * 100) + "px";
+//         car2.style.marginleft = (Math.floor.random() * 100) + "px";
+
+//         let soma1 = parseInt(car1.style.marginLeft)
+//         car1.style.marginLeft = Math.floor(parseInt(car1.style.marginLeft) + Math.floor(Math.random() * 100) + "px;"
+
+
+// });
