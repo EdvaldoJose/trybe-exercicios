2b95feee98d1419898155bc1d300856c677fdb7b
EdvaldoJose <edvaldojose003@gmail.com>
2022-04-20 12:37:01 -0300

    Atualização dos exercicios

---
+++ Fundamentos/bloco-05-javascript-dom-e-seletores/dia-01-javascript-dom-e-seletores/script.js
@@ -0,0 +1,24 @@
+const paragraph = document.getElementById("paragraph");
+    paragraph.style.color = "red";
+
+console.log(document.getElementById("teste").innerText);
+
+document.getElementById("teste").innerText = "Entrei na trybe para me tornar um grande programado web";
+
+document.getElementById("page-title").innerText = "Avangers"
+
+document.getElementById("second-paragraph").innerText = "Vou estudar muito para entender profundamentos esse assunto e dominar essas incríveis ferramenta HTML/CSS/JS."
+
+domentre = document.getElementById("subtitle").innerText = "Vamos caminha juntos com a Trybe vamo-que-vamo!"
+
+document.getElementsByClassName("piloto-f1-atual")[0
+].innerText = "Stroll";
+
+let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual");
+    
+    for (let i = 0; i < pilotosDeF1.length; i += 1) {
+        pilotosDeF1[i].innerText = 'Lews Hamilton'
+    };
+console.log(pilotosDeF1[i]);
+
+
